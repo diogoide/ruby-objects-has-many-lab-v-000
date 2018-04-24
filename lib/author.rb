b8e21274b56746a6b46dda7abfd656f0057782d0
Diogo Ide <diogoide@gmail.com>
2018-04-24 00:33:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -16,10 +16,10 @@ class Author
   end
 
   def add_post_by_title(post_title)
-    post = Post.new
+    post = Post.new(post_title)
     post.name = post_title
     post.author = self
-    @posts << post 
+    @posts << post
   end
 
 end
