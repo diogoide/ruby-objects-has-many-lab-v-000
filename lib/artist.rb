9fbe0691c302c7730529b24eff7c9a24f6fba9ab
Diogo Ide <diogoide@gmail.com>
2018-04-24 00:03:15 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,10 +6,12 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    @@songs = []
   end
 
   def add_song(song)
     @songs << song
+    @@songs << song
     song.artist = self
   end
 
@@ -20,10 +22,11 @@ class Artist
   def add_song_by_name(song_name)
     song = Song.new(song_name)
     @songs << song
+    @@songs << song
     song.artist = self
   end
 
   def song_count
-    @songs.size
+    @@songs.size
   end
 end
